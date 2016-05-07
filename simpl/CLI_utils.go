754ab44ba0f231110d4036b61adff652ca6f92e7
tuxxy <me@johnpacific.com>
2016-05-06 21:32:23 -0700

    Add GetKey Function
    GetKey allows secure entry of sensitive info and allows low level acces
    to associated input bye slice.

--- simpl/CLI_utils.go
+++ simpl/CLI_utils.go
@@ -4,4 +4,15 @@ import "golang.org/x/crypto/ssh/terminal"
 
 type CLI struct {
     Input []byte
+    Term terminal.Terminal
+}
+
+func (c *CLI) GetKey() {
+    // Can't put a value directly into the struct, see:
+    // https://github.com/golang/go/issues/6842
+    key, err := terminal.ReadPassword(0)
+    if err != nil {
+        panic(err)
+    }
+    c.Input = key[0:]
 }
