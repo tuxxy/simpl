2471630126ec63eabf8e71fb39b99f1cd5d2aa28
tux <tux@xmission.com>
2016-05-11 22:47:35 -0600

    Add ConfirmPrompt method for boolean questions

--- simpl/CLI.go
+++ simpl/CLI.go
@@ -5,16 +5,20 @@ import (
     "bufio"
     "os"
     "fmt"
+    "strings"
+    "errors"
 )
 
 type CLI struct {
     Input []byte
     Reader *bufio.Reader
+    validChoices *map[string]bool
 }
 
 func InitCLI() (*CLI) {
     reader := bufio.NewReader(os.Stdin)
-    return &CLI{nil, reader}
+    return &CLI{nil, reader,
+        &map[string]bool{"y": true, "yes": true, "n": false, "no": false}}
 }
 
 func (c *CLI) SecureGetInput() {
@@ -33,4 +37,14 @@ func (c *CLI) GetInput() {
         panic(err)
     }
     c.Input = data[0:]
+}
+
+func (c *CLI) ConfirmPrompt() (bool, error) {
+    c.GetInput()
+    choice := strings.ToLower(string(c.Input))
+    if (*c.validChoices)[choice] {
+        return (*c.validChoices)[choice], nil
+    } else {
+        return false, errors.New("Input was invalid - try again.")
+    }
 }
