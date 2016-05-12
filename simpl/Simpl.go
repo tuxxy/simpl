42434e9cb4cd28d98cd1860d8e09bfc8d2ad61fa
tux <tux@xmission.com>
2016-05-12 03:09:28 -0600

    Remove functionMap and use a switch instead

--- simpl/Simpl.go
+++ simpl/Simpl.go
@@ -1,14 +1,41 @@
 package main
 
+import (
+    "strings"
+    "fmt"
+    "os"
+)
+
 type Simpl struct {
     cli *CLI
 }
 
-func InitSimpl() {
+func InitSimpl() *Simpl {
     cli := InitCLI()
     return &Simpl{cli}
 }
 
-func (s *Simpl) Run() {
+func (s *Simpl) Cleanup() int {
+    // TODO Wipe memory, etc
+    return 0
+}
 
+func (s *Simpl) Run() {
+    isRunning := true
+    for isRunning {
+        s.cli.GetInput()
+        terms := strings.Split(strings.TrimSpace(string(s.cli.Input)), " ")
+        switch terms[0] {
+        case "add", "touch", "new", "create":
+            s.AddEntry(terms)
+        case "help", "?":
+            s.cli.DisplayHelp()
+        case "exit", ":q", ":wq":
+            os.Exit(s.Cleanup())    
+        }
+    }
+}
+
+func (s *Simpl) AddEntry(terms []string) {
+    fmt.Println("I ran!", terms)
 }
--- simpl/main.go
+++ simpl/main.go
@@ -2,4 +2,6 @@ package main
 
 func main() {
     SanityCheck()
+    simpl := InitSimpl()
+    simpl.Run()
 }
