78a0fdeaf966f3378703da6bf44bc474aba7612f
tux <tux@xmission.com>
2016-05-11 22:12:53 -0600

    Fix checksum validation sentence formatting

--- simpl/crypto.go
+++ simpl/crypto.go
@@ -19,20 +19,20 @@ func SanityCheck() bool {
     selfSum := hex.EncodeToString(hash.Sum([]byte{}))
     resp, err := http.Get("https://raw.githubusercontent.com/tuxxy/simpl/go/CHECKSUM")
     if err != nil {
-        fmt.Println("%s -- Error grabbing current simpl checksum! It is recommended to verify the checksum manually before continuing.", err)
+        fmt.Println("%s -- Error grabbing current simpl checksum! It is recommended to verify the checksum manually before continuing.\n\n", err)
         return false
     }
     defer resp.Body.Close()
     body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        fmt.Println("%s -- Error grabbing current simpl checksum! It is recommended to verify the checksum manually before continuing.", err)
+        fmt.Println("%s -- Error grabbing current simpl checksum! It is recommended to verify the checksum manually before continuing.\n\n", err)
         return false
     }
     if selfSum == string(body[:len(body)-1]) {
-        fmt.Println("Checksum verification SUCCESS!")
+        fmt.Println("Checksum verification SUCCESS!\n\n")
         return true
     } else {
-        fmt.Println("Checksum verification FAILED! It is recommended to not continue until this verification succeeds with the checksum from: https://raw.githubusercontent.com/tuxxy/simpl/go/CHECKSUM")
+        fmt.Println("Checksum verification FAILED! It is recommended to not continue until this verification succeeds with the checksum from https://raw.githubusercontent.com/tuxxy/simpl/go/CHECKSUM\n\n")
         return false
     }
 }
