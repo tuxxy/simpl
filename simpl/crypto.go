d380a1582f4dd5f4118dcd4e857d6a1d0c8e98e0
tux <me@johnpacific.com>
2016-05-26 01:56:24 -0600

    Remove useless goroutine

--- simpl/crypto.go
+++ simpl/crypto.go
@@ -21,7 +21,7 @@ func DeriveKey(passphrase []byte) []byte {
 	// I've found that 17 rounds is a reasonable wait for assured security.
 	key, err := bcrypt.GenerateFromPassword(hashPass[:], 17)
 	if err != nil {
-		go ZeroData(hashPass[:])
+		ZeroData(hashPass[:])
 		panic(err)
 	}
 	return key[:]
