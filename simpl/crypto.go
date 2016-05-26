450abb536d6c447c9a2756bbf2637a76597cc6e4
tux <me@johnpacific.com>
2016-05-25 21:30:26 -0600

    Remove SanitCheck()
    Removing SanityCheck for now. I'll add something similar (and better) in a future version.

--- simpl/crypto.go
+++ simpl/crypto.go
@@ -1,34 +1,6 @@
 package main
 
 import (
-    "crypto/sha512"
-    "os"
-    "io/ioutil"
-    "fmt"
-    "encoding/hex"
-    "net/http"
 )
 
-func SanityCheck() {
-    f, err := ioutil.ReadFile(os.Args[0])
-    if err != nil {
-        panic(err)
-    }
-    hash := sha512.New()
-    hash.Write(f)
-    selfSum := hex.EncodeToString(hash.Sum([]byte{}))
-    resp, err := http.Get("https://raw.githubusercontent.com/tuxxy/simpl/go/CHECKSUM")
-    if err != nil {
-        fmt.Println("%s -- Error grabbing current simpl checksum! It is recommended to verify the checksum manually before continuing.\n\n", err)
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        fmt.Println("%s -- Error grabbing current simpl checksum! It is recommended to verify the checksum manually before continuing.\n\n", err)
-    }
-    if selfSum == string(body[:len(body)-1]) {
-        fmt.Println("Checksum verification SUCCESS!\n\n")
-    } else {
-        fmt.Println("Checksum verification FAILED! It is recommended to not continue until this verification succeeds with the checksum from https://raw.githubusercontent.com/tuxxy/simpl/go/CHECKSUM\n\n")
-    }
-}
+
--- simpl/main.go
+++ simpl/main.go
@@ -1,7 +1,6 @@
 package main
 
 func main() {
-    SanityCheck()
     simpl := InitSimpl()
     simpl.Run()
 }
