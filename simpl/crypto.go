7a31a88639202bd5ca224bd81696dd63f7d2a45f
tux <me@johnpacific.com>
2016-05-26 01:37:44 -0600

    Add functions for deriving they key and zeroing memory

--- simpl/crypto.go
+++ simpl/crypto.go
@@ -1,6 +1,28 @@
 package main
 
 import (
+	"crypto/bcrypt"
+	"crypto/sha512"
 )
 
+func ZeroData(data []byte) {
+	// Zeros the buffer
+	// DOES NOT GUARANTEE SAFETY
+	for i := range data[:] {
+		data[i] = byte(0)
+	}
+}
 
+func DeriveKey(passphrase []byte) []byte {
+	// The maximum pass length is 72 chars, thus all passowrds will benefit
+	// from a sha512 hash for maximum entropy allowance.
+	hashPass := sha512.New().Sum(passphrase[:])
+	go ZeroData(passphrase[:])
+	// I've found that 17 rounds is a reasonable wait for assured security.
+	key, err := bcrypt.GenerateFromPassword(hashPass[:], 17)
+	if err != nil {
+		go ZeroData(hashPass[:])
+		panic(err)
+	}
+	return key[:]
+}
